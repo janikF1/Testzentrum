990765d7880fa8e83cfc5514d3dbafb0793050f1
janikF1 <jolograggy45@gmail.com>
2022-04-08 18:49:06 +0200

    testzentrum grobe Struktur

--- tetzentrum.c
+++ tetzentrum.c
@@ -1,1 +1,49 @@
-#test
+#include <windows.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+
+int main()
+{
+    // create file with free timeslots
+    // while True():
+    // {
+    //     track directory
+    //     if(new file in directory)
+    //     {
+    //         open file
+    //         read email-adresse
+    //         read time 
+    //         if(mail is valid && time is valid)
+    //         {
+    //             enter this person in time file
+    //         }
+    //         else
+    //         {
+    //             write denied to file
+    //         }
+    //     }
+    //     if(file deleted)
+    //     {
+    //         delete booked test
+    //     }
+    //     if(strg+c pressed)
+    //     {
+    //         for(i in booked timeslots)
+    //         {
+    //             random true or false
+    //             if(random == true)
+    //             {
+    //                 write positive in user file
+    //                 add person to list with positive tests
+    //             }
+    //             else if(random == negativ)
+    //             {
+    //                 write negative to file
+    //             }
+    //         }
+    //     }
+    // }
+    return 0;
+}
